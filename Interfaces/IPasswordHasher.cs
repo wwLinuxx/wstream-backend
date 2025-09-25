19920bba8d0a621f343ecb6a320113d72ed5976a
wwlinuxx <wwlinuxoff@gmail.com>
2025-09-25 18:25:59 +0500

    Added PasswordHasher

---
+++ Interfaces/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace UzTube.Interfaces;
+
+public interface IPasswordHasher
+{
+    public string Encrypt(string password, string salt);
+    public bool Verify(string passwordHash, string password, string salt);
+}
