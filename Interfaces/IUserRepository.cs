759ff2301fda1d101b98b32c2b762af99fb41418
wwlinuxx <wwlinuxoff@gmail.com>
2025-09-25 18:26:17 +0500

    Added UserRepository

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using UzTube.Models.DTO;
+using UzTube.Services;
+
+namespace UzTube.Interfaces
+{
+    public interface IUserRepository
+    {
+        Result Login(LoginDTO dto);
+        Result Register(RegisterDTO dto);
+    }
+}
