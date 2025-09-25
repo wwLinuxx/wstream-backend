35a8af226085dcc281b59e7d49fbcdade173dfe9
wwlinuxx <wwlinuxoff@gmail.com>
2025-09-25 18:25:42 +0500

    Removed Username from OnModelCreating

--- Migrations/20250925130418_Remove_Username_From_UserEntity.Designer.cs
+++ Migrations/20250925130418_Remove_Username_From_UserEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UzTube.Database;
@@ -11,9 +12,11 @@ using UzTube.Database;
 namespace UzTube.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250925130418_Remove_Username_From_UserEntity")]
+    partial class Remove_Username_From_UserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
