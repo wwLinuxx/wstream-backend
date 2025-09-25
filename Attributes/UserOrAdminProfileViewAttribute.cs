ab41cd8259f0b6a9e221cf17043217f8e1cc24ab
wwlinuxx <wwlinuxoff@gmail.com>
2025-09-25 21:46:55 +0500

    Add UserOrAdminProfileViewAttribute

---
+++ Attributes/UserOrAdminProfileViewAttribute.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using System.Text.Json;
+using UzTube.Services;
+
+namespace UzTube.Attributes;
+
+public class UserOrAdminProfileViewAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        ClaimsPrincipal user = context.HttpContext.User;
+
+        if (user.Identity == null || !user.Identity.IsAuthenticated)
+        {
+            context.Result = new Result
+            {
+                Message = "Unauthorized",
+                StatusCode = 401
+            }; 
+            
+            return;
+        }
+
+        string? userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+        string? requestUserId = context.HttpContext.Request.RouteValues["userId"]?.ToString();
+
+        string rolesJson = user.FindFirstValue(ClaimTypes.Role);
+
+        if (rolesJson != null)
+        {
+            List<string> roles = JsonSerializer.Deserialize<List<string>>(rolesJson);
+            bool hasRole = roles.Contains("Admin") || roles.Contains("root");
+
+            if (hasRole) return;
+        }
+
+        if (userId != requestUserId)
+        {
+            context.Result = new Result
+            {
+                Message = "Forbidden",
+                StatusCode = 403
+            };
+
+            return;
+        }
+    }
+}
