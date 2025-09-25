5d860455d0a6193381ccabf297530b821c9e3d32
wwlinuxx <wwlinuxoff@gmail.com>
2025-09-25 16:40:03 +0500

    Added LoginDTO

---
+++ Models/DTO/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UzTube.Models.DTO;
+
+public record LoginDTO
+{
+    [Required]
+    [StringLength(maximumLength: 30, MinimumLength = 5, ErrorMessage = "Username most be minimum legth 5 maximum 30")]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(maximumLength: 30, MinimumLength = 5, ErrorMessage = "Password most be minimum legth 5 maximum 30")]
+    public string Password { get; set; }
+}
