a13b31aaa7ac48bd3506863ecb8cfb316ca5b198
wwlinuxx <wwlinuxoff@gmail.com>
2025-09-25 15:21:54 +0500

    Add UserService

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using UzTube.Entities;
+
+namespace UzTube.Interfaces;
+
+public interface IUserService
+{
+    bool Exists(int userId);
+    User GetById(int userId);
+    IEnumerable<User> Users();
+    IEnumerable<string> Roles();
+    IEnumerable<string> Roles(int userId);
+    IEnumerable<string> Permissions(int userId);
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using UzTube.Database;
+using UzTube.Entities;
+using UzTube.Interfaces;
+
+namespace UzTube.Services;
+
+public class UserService : IUserService
+{
+    private readonly AppDbContext _context;
+
+    public UserService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public bool Exists(int userId)
+        => _context.Users.Any(u => u.Id == userId);
+
+    public User GetById(int userId)
+        => _context.Users.FirstOrDefault(u => u.Id == userId);
+
+    public IEnumerable<User> Users()
+        => _context.Users;
+
+    public IEnumerable<string> Permissions(int userId)
+    {
+        return _context.UserRoles
+            .Where(ur => ur.UserId == userId)
+            .SelectMany(ur => ur.Role.RolePermissions)
+            .Select(rp => rp.Permission.Name)
+            .Distinct();
+    }
+
+    public IEnumerable<string> Roles()
+    {
+        return _context.UserRoles
+            .Select(ur => ur.Role.Name)
+            .Distinct();
+    }
+
+    public IEnumerable<string> Roles(int userId)
+    {
+        return _context.UserRoles
+            .Where(ur => ur.UserId == userId)
+            .Select(ur => ur.Role.Name)
+            .Distinct();
+    }
+
+}
