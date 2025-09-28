35c131e3f3b1eec2810798c1be642a4839988073
wwLinux <wwlinuxoff@gmail.com>
2025-09-28 18:48:49 +0500

    Added NotZeroAttribute

---
+++ Attributes/NotZeroAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UzTube.Attributes;
+
+public class NotZeroAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        ErrorMessage = "UserId most be under 0";
+
+        if (value is int number && number > 0)
+            return true;
+        return false;
+    }
+}
