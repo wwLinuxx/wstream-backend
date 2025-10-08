89c925f4dbd41f28c62087c37ae9b5a8931a8304
wwLinux <wwlinuxoff@gmail.com>
2025-10-08 21:02:14 +0500

    Init Min.io

---
+++ Models/MinioOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace UzTube.Models;
+
+public class MinioOptions
+{
+    public string Endpoint { get; set; }
+
+    public string AccessKey { get; set; }
+
+    public string SecretKey { get; set; }
+
+    public bool UseSSL { get; set; }
+}
