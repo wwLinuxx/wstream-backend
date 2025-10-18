6fff2399264172cf82e2739defe41ddd1ac2dc32
wwLinux <wwlinuxoff@gmail.com>
2025-10-18 23:21:16 +0500

    Validators redesigned code

--- src/UzTube.Application/Models/Validators/User/CreateUserValidator.cs
+++ src/UzTube.Application/Models/Validators/User/CreateUserValidator.cs
@@ -14,45 +14,45 @@ public class CreateUserValidator : AbstractValidator<CreateUserModel>
 
         RuleFor(u => u.Email)
             .MinimumLength(UserValidatorConfiguration.MinimumEmailLength)
-            .WithMessage($"Email should have minimum {UserValidatorConfiguration.MinimumEmailLength} characters")
+                .WithMessage($"Email should have minimum {UserValidatorConfiguration.MinimumEmailLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumEmailLength)
-            .WithMessage($"Email should have maximum {UserValidatorConfiguration.MaximumEmailLength} characters")
+                .WithMessage($"Email should have maximum {UserValidatorConfiguration.MaximumEmailLength} characters")
             .Must(EmailAddressIsUnique)
-            .WithMessage("Email address is already in use");
+                .WithMessage("Email address is already in use");
 
         RuleFor(u => u.Password)
             .MinimumLength(UserValidatorConfiguration.MinimumPasswordLength)
-            .WithMessage($"Password should have minimum {UserValidatorConfiguration.MinimumPasswordLength} characters")
+                .WithMessage($"Password should have minimum {UserValidatorConfiguration.MinimumPasswordLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumPasswordLength)
-            .WithMessage($"Password should have maximum {UserValidatorConfiguration.MaximumPasswordLength} characters");
+                .WithMessage($"Password should have maximum {UserValidatorConfiguration.MaximumPasswordLength} characters");
 
         RuleFor(u => u.PhoneNumber)
             .MinimumLength(UserValidatorConfiguration.MinimumPhoneNumberLength)
-            .WithMessage($"Phone number should have minimum {UserValidatorConfiguration.MinimumPhoneNumberLength} characters")
+                .WithMessage($"Phone number should have minimum {UserValidatorConfiguration.MinimumPhoneNumberLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumPhoneNumberLength)
-            .WithMessage($"Phone number should have maximum {UserValidatorConfiguration.MaximumPhoneNumberLength} characters")
+                .WithMessage($"Phone number should have maximum {UserValidatorConfiguration.MaximumPhoneNumberLength} characters")
             .Must(PhoneNumberIsUnique)
-            .WithMessage("Phone number is already in user");
+                .WithMessage("Phone number is already in user");
 
         RuleFor(u => u.FirstName)
             .MinimumLength(UserValidatorConfiguration.MinimumFirstNameLength)
-            .WithMessage($"FirstName should have minimum {UserValidatorConfiguration.MinimumFirstNameLength} characters")
+                .WithMessage($"FirstName should have minimum {UserValidatorConfiguration.MinimumFirstNameLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumFirstNameLength)
-            .WithMessage($"FirstName should have maximum {UserValidatorConfiguration.MaximumFirstNameLength} characters");
+                .WithMessage($"FirstName should have maximum {UserValidatorConfiguration.MaximumFirstNameLength} characters");
 
         RuleFor(u => u.LastName)
             .MinimumLength(UserValidatorConfiguration.MinimumLastNameLength)
-            .WithMessage($"LastName should have minimum {UserValidatorConfiguration.MinimumLastNameLength}")
+                .WithMessage($"LastName should have minimum {UserValidatorConfiguration.MinimumLastNameLength}")
             .MaximumLength(UserValidatorConfiguration.MaximumLastNameLength)
-            .WithMessage($"LastName should have maximum {UserValidatorConfiguration.MaximumLastNameLength} characters");
+                .WithMessage($"LastName should have maximum {UserValidatorConfiguration.MaximumLastNameLength} characters");
 
         RuleFor(u => u.Age)
             .InclusiveBetween(UserValidatorConfiguration.MinimumAge, UserValidatorConfiguration.MaximumAge)
-            .WithMessage($"Age must be between {UserValidatorConfiguration.MinimumAge} and {UserValidatorConfiguration.MaximumEmailLength} years old.");
+                .WithMessage($"Age must be between {UserValidatorConfiguration.MinimumAge} and {UserValidatorConfiguration.MaximumEmailLength} years old.");
 
         RuleFor(u => u.CountryId)
             .NotEmpty()
-            .WithMessage("Email address is not valid");
+                .WithMessage("Email address is not valid");
     }
 
     private bool EmailAddressIsUnique(string email)
--- src/UzTube.Application/Models/Validators/User/LoginValidator.cs
+++ src/UzTube.Application/Models/Validators/User/LoginValidator.cs
@@ -14,14 +14,14 @@ public class LoginValidator : AbstractValidator<LoginUserModel>
 
         RuleFor(u => u.Email)
             .MinimumLength(UserValidatorConfiguration.MinimumEmailLength)
-            .WithMessage($"Email address should have minimum {UserValidatorConfiguration.MinimumEmailLength} characters")
+                .WithMessage($"Email address should have minimum {UserValidatorConfiguration.MinimumEmailLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumEmailLength)
-            .WithMessage($"Email address should have maximum {UserValidatorConfiguration.MaximumEmailLength} characters"); 
+                .WithMessage($"Email address should have maximum {UserValidatorConfiguration.MaximumEmailLength} characters"); 
 
         RuleFor(u => u.Password)
             .MinimumLength(UserValidatorConfiguration.MinimumPasswordLength)
-            .WithMessage($"Password should have minimum {UserValidatorConfiguration.MinimumPasswordLength} characters")
+                .WithMessage($"Password should have minimum {UserValidatorConfiguration.MinimumPasswordLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumPasswordLength)
-            .WithMessage($"Password should have maximum {UserValidatorConfiguration.MaximumPasswordLength} characters");
+                .WithMessage($"Password should have maximum {UserValidatorConfiguration.MaximumPasswordLength} characters");
     }
 }
