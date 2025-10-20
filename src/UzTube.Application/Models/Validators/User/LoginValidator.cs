d68b2e90e0aba3e114f1f2fc676b1e950d86a981
wwLinux <wwlinuxoff@gmail.com>
2025-10-20 15:59:36 +0500

    Added Email constraint

--- src/UzTube.Application/Models/Validators/User/LoginValidator.cs
+++ src/UzTube.Application/Models/Validators/User/LoginValidator.cs
@@ -14,14 +14,16 @@ public class LoginValidator : AbstractValidator<LoginUserModel>
 
         RuleFor(u => u.Email)
             .MinimumLength(UserValidatorConfiguration.MinimumEmailLength)
-                .WithMessage($"Email address should have minimum {UserValidatorConfiguration.MinimumEmailLength} characters")
+            .WithMessage($"Email address should have minimum {UserValidatorConfiguration.MinimumEmailLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumEmailLength)
-                .WithMessage($"Email address should have maximum {UserValidatorConfiguration.MaximumEmailLength} characters"); 
+            .WithMessage($"Email address should have maximum {UserValidatorConfiguration.MaximumEmailLength} characters")
+            .EmailAddress()
+            .WithMessage("Email address is not valid"); 
 
         RuleFor(u => u.Password)
             .MinimumLength(UserValidatorConfiguration.MinimumPasswordLength)
-                .WithMessage($"Password should have minimum {UserValidatorConfiguration.MinimumPasswordLength} characters")
+            .WithMessage($"Password should have minimum {UserValidatorConfiguration.MinimumPasswordLength} characters")
             .MaximumLength(UserValidatorConfiguration.MaximumPasswordLength)
-                .WithMessage($"Password should have maximum {UserValidatorConfiguration.MaximumPasswordLength} characters");
+            .WithMessage($"Password should have maximum {UserValidatorConfiguration.MaximumPasswordLength} characters");
     }
 }
