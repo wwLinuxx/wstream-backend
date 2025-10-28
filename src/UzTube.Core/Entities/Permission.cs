1dbab5c1248ff450fb425e26e1898c927ecbde52
wwLinux <wwlinuxoff@gmail.com>
2025-10-28 16:08:22 +0500

    Added UpdatedOn property

--- src/UzTube.Core/Entities/Permission.cs
+++ src/UzTube.Core/Entities/Permission.cs
@@ -7,8 +7,8 @@ public class Permission
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
-
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedOn { get; set; }
 
     public ICollection<RolePermission> RolePermissions { get; set; } = [];
 }
